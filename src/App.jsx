680cdc6c8909c8d341bb900feda295d676ca9241
inatbalthazar <inatbalthazar@gmail.com>
2026-08-20 16:48:54 +0800

    style(App.jsx): remove stray blank line and trim trailing whitespace

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,6 @@ import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
-
   return (
     <>
       <section id="center">
@@ -118,5 +117,7 @@ function App() {
     </>
   )
 }
+
+
 
 export default App
